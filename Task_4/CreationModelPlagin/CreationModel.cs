c4a76b94685f2e98cca3ac32f7abc16eac977fca
Федор Александров <fedfed95@yandex.ru>
2022-02-06 23:22:23 +0300

    Revert "Task_5"
    This reverts commit a771243e13753e83f0b7b482b37234f06e85acb5.

--- Task_4/CreationModelPlagin/CreationModel.cs
+++ Task_4/CreationModelPlagin/CreationModel.cs
@@ -1,6 +1,5 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -34,20 +33,13 @@ namespace CreationModelPlagin
             double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
             double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
 
-            Transaction tr = new Transaction(doc, "Построение домика");
-            tr.Start();
-            List<Wall> walls = CreateFourWalls(doc, width, depth, level1, level2);
-            AddDoor(doc, level1, walls[0]);
-            AddWindow(doc, level1, walls[1]);
-            AddWindow(doc, level1, walls[2]);
-            AddWindow(doc, level1, walls[3]);
-
-            tr.Commit();
+            CreateFourWalls(doc, width, depth, level1, level2);
+            
 
             return Result.Succeeded;
         }
 
-        public List<Wall> CreateFourWalls (Document doc,double width, double depth, Level botton, Level top)
+        public void CreateFourWalls (Document doc,double width, double depth, Level botton, Level top)
         {
             double dx = width / 2;
             double dy = depth / 2;
@@ -61,6 +53,8 @@ namespace CreationModelPlagin
 
             List<Wall> walls = new List<Wall>();
 
+            Transaction tr = new Transaction(doc, "Построение стен");
+            tr.Start();
             for (int i = 0; i < 4; i++)
             {
                 Line line = Line.CreateBound(points[i], points[i + 1]);
@@ -68,61 +62,7 @@ namespace CreationModelPlagin
                 walls.Add(wall);
                 wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(top.Id);
             }
-            return walls;
-        }
-
-        private void AddDoor(Document doc, Level level1, Wall wall)
-        {
-
-            FamilySymbol doorType = new FilteredElementCollector(doc)
-                .OfClass(typeof(FamilySymbol))
-                .OfCategory(BuiltInCategory.OST_Doors)
-                .OfType<FamilySymbol>()
-                .Where(x => x.Name == "0915 x 2134 мм")
-                .Where(x => x.Family.Name == "Одиночные-Щитовые")
-                .FirstOrDefault();
-
-
-            LocationCurve hostCurve = wall.Location as LocationCurve;
-            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
-            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
-            XYZ point = (point1 + point2) / 2;
-
-            if (!doorType.IsActive)
-            {
-                doorType.Activate();
-            }
-
-            doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
-        }
-
-        private void AddWindow(Document doc, Level level1, Wall wall)
-        {
-
-            FamilySymbol windowType = new FilteredElementCollector(doc)
-                .OfClass(typeof(FamilySymbol))
-                .OfCategory(BuiltInCategory.OST_Windows)
-                .OfType<FamilySymbol>()
-                .Where(x => x.Name == "0915 x 1830 мм")
-                .Where(x => x.Family.Name == "Фиксированные")
-                .FirstOrDefault();
-
-
-            LocationCurve hostCurve = wall.Location as LocationCurve;
-            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
-            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
-            XYZ point = (point1 + point2) / 2;
-
-            if (!windowType.IsActive)
-            {
-                windowType.Activate();
-            }
-
-            FamilyInstance familyInstance =  doc.Create.NewFamilyInstance(point, windowType, wall, level1, StructuralType.NonStructural);
-
-            double height  = UnitUtils.ConvertToInternalUnits(1000, UnitTypeId.Millimeters);
-            familyInstance.get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM).Set(height);
-            familyInstance.flipFacing();
+            tr.Commit();
 
         }
     }
